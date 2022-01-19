e347a73af7be562f58b909f4a33cccfb8ba433ec
Luis Felipe Fabiane <luisffabiane@gmail.com>
2022-01-18 21:17:43 -0300

    Gerenciamento de Clientes e Funcionários
    Nesse commit foram adicionadas as opções de gerencia (cadastrar, editar, excluir e listar) para as entidades de pessoas

--- controle/controlador_pessoa.py
+++ controle/controlador_pessoa.py
@@ -1,31 +1,137 @@
 from limite.tela_pessoa import TelaPessoa
 from entidade.cliente import Cliente
+from entidade.funcionario import Funcionario
 
 class ControladorPessoa:
 
     def __init__(self, controlador_cadastro):
         self.__clientes = []
+        self.__qtde_clientes = 0
         self.__funcionarios = []
+        self.__qtde_funcionarios = 0
         self.__tela_pessoa = TelaPessoa()
         self.__controlador_cadastro = controlador_cadastro
 
+    def pega_cliente_por_cpf(self, cpf:int):
+        for cliente in self.__clientes:
+            if cliente.cpf == cpf:
+                return cliente
+        return None
+
+    def pega_funcionario_por_cpf(self, cpf:int):
+        for funcionario in self.__funcionarios:
+            if funcionario.cpf == cpf:
+                return funcionario
+        return None
+
     def incluir_cliente(self):
-        codigo = len(self.__clientes) + 1
+        self.__qtde_clientes += 1
+        codigo = self.__qtde_clientes
         dados_cliente = self.__tela_pessoa.pega_dados_cliente()
-        cliente = Cliente(codigo, dados_cliente["nome"], dados_cliente["data_nascimento"], dados_cliente["cpf"], dados_cliente["senha_contas"])
+        cliente = Cliente(codigo, dados_cliente["nome"], dados_cliente["data_nascimento"], dados_cliente["cpf"], dados_cliente["senha_operacoes"])
         self.__clientes.append(cliente)
+        self.__tela_pessoa.mostra_mensagem("Cliente cadastrado com sucesso!")
+
+    def alterar_cliente(self):
+        cpf_cliente = self.__tela_pessoa.seleciona_cpf()
+        cliente = self.pega_cliente_por_cpf(cpf_cliente)
+        if (cliente is not None):
+            senha_cliente = self.__tela_pessoa.pega_senha_pesoa()
+            if cliente.senha_operacoes == senha_cliente:
+                novos_dados_cliente = self.__tela_pessoa.pega_dados_cliente_alteracao()
+                cliente.nome = novos_dados_cliente["nome"]
+                cliente.senha_operacoes = novos_dados_cliente["senha_operacoes"]
+                self.__tela_pessoa.mostra_mensagem("Cliente atualizado com sucesso!")
+            else:
+                self.__tela_pessoa.mostra_mensagem("ATENÇÃO: Senha incorreta!")
+        else:
+            self.__tela_pessoa.mostra_mensagem("ATENÇÃO: Cliente não existente!")
+
+    def excluir_cliente(self):
+        cpf_cliente = self.__tela_pessoa.seleciona_cpf()
+        cliente = self.pega_cliente_por_cpf(cpf_cliente)
+        if (cliente is not None):
+            senha_cliente = self.__tela_pessoa.pega_senha_pesoa()
+            if cliente.senha_operacoes == senha_cliente:
+                self.__clientes.remove(cliente)
+                self.__tela_pessoa.mostra_mensagem("Cliente excluido com sucesso!")
+            else:
+                self.__tela_pessoa.mostra_mensagem("ATENÇÃO: Senha incorreta!")
+        else:
+            self.__tela_pessoa.mostra_mensagem("ATENÇÃO: Cliente não existente!")
+
+    def lista_cliente(self):
+        cpf_cliente = self.__tela_pessoa.seleciona_cpf()
+        cliente = self.pega_cliente_por_cpf(cpf_cliente)
+        if (cliente is not None):
+            senha_cliente = self.__tela_pessoa.pega_senha_pesoa()
+            if cliente.senha_operacoes == senha_cliente:
+                dados_cliente = {"codigo": cliente.codigo, "nome": cliente.nome,
+                                 "data_nascimento": cliente.data_nascimento, "cpf": cliente.cpf}
+                self.__tela_pessoa.lista_cliente(dados_cliente)
+            else:
+                self.__tela_pessoa.mostra_mensagem("ATENÇÃO: Senha incorreta!")
+        else:
+            self.__tela_pessoa.mostra_mensagem("ATENÇÃO: Cliente não existente!")
 
     def incluir_funcionario(self):
-        codigo = len(self.__funcionarios) + 1
+        self.__qtde_funcionarios += 1
+        codigo = self.__qtde_funcionarios
         dados_funcionario = self.__tela_pessoa.pega_dados_funcionario()
-        cliente = Cliente(codigo, dados_funcionario["nome"], dados_funcionario["data_nascimento"], dados_funcionario["cpf"], dados_funcionario["numero_CTPS"], dados_funcionario["senha_funcionario"])
-        self.__clientes.append(cliente)
+        funcionario = Funcionario(codigo, dados_funcionario["nome"], dados_funcionario["data_nascimento"], dados_funcionario["cpf"], dados_funcionario["numero_CTPS"], dados_funcionario["senha_funcionario"])
+        self.__funcionarios.append(funcionario)
+
+    def alterar_funcionario(self):
+        cpf_funcionario = self.__tela_pessoa.seleciona_cpf()
+        funcionario = self.pega_funcionario_por_cpf(cpf_funcionario)
+        if (funcionario is not None):
+            senha_funcionario = self.__tela_pessoa.pega_senha_pessoa()
+            if funcionario.senha_operacoes == senha_funcionario:
+                novos_dados_funcionario = self.__tela_pessoa.pega_dados_funcionario_alteracao()
+                funcionario.nome = novos_dados_funcionario["nome"]
+                funcionario.senha_funcionario = novos_dados_funcionario["senha_funcionario"]
+                self.__tela_pessoa.mostra_mensagem("Funcionario atualizado com sucesso!")
+            else:
+                self.__tela_pessoa.mostra_mensagem("ATENÇÃO: Senha incorreta!")
+        else:
+            self.__tela_pessoa.mostra_mensagem("ATENÇÃO: Funcionário não existente!")
+
+    def excluir_funcionario(self):
+        cpf_funcionario = self.__tela_pessoa.seleciona_cpf()
+        funcionario = self.pega_funcionario_por_cpf(cpf_funcionario)
+        if (funcionario is not None):
+            senha_funcionario = self.__tela_pessoa.pega_senha_pesoa()
+            if funcionario.senha_funcionario == senha_funcionario:
+                self.__funcionario.remove(funcionario)
+                self.__tela_pessoa.mostra_mensagem("Funcionário excluido com sucesso!")
+            else:
+                self.__tela_pessoa.mostra_mensagem("ATENÇÃO: Senha incorreta!")
+        else:
+            self.__tela_pessoa.mostra_mensagem("ATENÇÃO: Funcionário não existente!")
+
+    def lista_funcionario(self):
+        cpf_funcionario = self.__tela_pessoa.seleciona_cpf()
+        funcionario = self.pega_funcionario_por_cpf(cpf_funcionario)
+        if (funcionario is not None):
+            senha_funcionario = self.__tela_pessoa.pega_senha_pesoa()
+            if funcionario.senha_funcionario == senha_funcionario:
+                dados_funcionario = {"codigo": funcionario.codigo, "nome": funcionario.nome,
+                                 "data_nascimento": funcionario.data_nascimento, "cpf": funcionario.cpf,
+                                 "numero_CTPS": funcionario.numero_CTPS}
+                self.__tela_pessoa.lista_funcionario(dados_funcionario)
+            else:
+                self.__tela_pessoa.mostra_mensagem("ATENÇÃO: Senha incorreta!")
+        else:
+            self.__tela_pessoa.mostra_mensagem("ATENÇÃO: Funcionário não existente!")
+
+    def listar_clientes(self):
+        print("lista clientes")
+        for cliente in self.__clientes:
+            dados_cliente = {"codigo":cliente.codigo, "nome":cliente.nome, "data_nascimento":cliente.data_nascimento, "cpf":cliente.cpf}
+            self.__tela_pessoa.lista_cliente(dados_cliente)
 
     def retorno_menu(self):
         self.__controlador_cadastro.abre_tela()
-
-    def teste(self):
-        print("\nEscolheu uma opção diferente de retornar ao menu anterior\n")
 
     def abre_tela(self):
         lista_opcoes = {1: self.abre_tela_cliente, 2: self.abre_tela_funcionario,
@@ -37,7 +143,8 @@ class ControladorPessoa:
             funcao_escolhida()
 
     def abre_tela_cliente(self):
-        lista_opcoes_cliente = {1: self.incluir_cliente, 2: self.teste, 3: self.teste,
+        lista_opcoes_cliente = {1: self.incluir_cliente, 2: self.alterar_cliente,
+                                3: self.excluir_cliente, 4: self.lista_cliente,
                                 0: self.abre_tela}
 
         while True:
@@ -46,7 +153,8 @@ class ControladorPessoa:
             funcao_escolhida()
 
     def abre_tela_funcionario(self):
-        lista_opcoes_funcionario = {1: self.incluir_funcionario, 2: self.teste, 3: self.teste,
+        lista_opcoes_funcionario = {1: self.incluir_funcionario, 2: self.alterar_funcionario,
+                                    3: self.excluir_funcionario, 4: self.lista_funcionario,
                                     0: self.abre_tela}
 
         while True:
--- limite/tela_pessoa.py
+++ limite/tela_pessoa.py
@@ -30,9 +30,16 @@ class TelaPessoa:
         nome = input("Nome: ")
         data_nascimento = input("Data de nascimento: ")
         cpf = cpf_valido()
-        senha_contas = input("Senha para operações: ")
+        senha_operacoes = input("Senha para operações: ")
 
-        return {"nome": nome, "data_nascimento": data_nascimento, "cpf": cpf, "senha_contas": senha_contas}
+        return {"nome": nome, "data_nascimento": data_nascimento, "cpf": cpf, "senha_operacoes": senha_operacoes}
+
+    def pega_dados_cliente_alteracao(self):
+        print("-------- Dados Atualizados do Cliente ----------")
+        nome = input("Nome: ")
+        senha_operacoes = input("Senha para operações: ")
+
+        return {"nome": nome, "senha_operacoes": senha_operacoes}
 
     def pega_dados_funcionario(self):
         print("-------- Dados do Funcionário ----------")
@@ -42,4 +49,33 @@ class TelaPessoa:
         numero_CTPS = input("Número da CTPS: ")
         senha_funcionario = input("Senha para operações: ")
 
-        return {"nome": nome, "data_nascimento": data_nascimento, "cpf": cpf, "numero_CTPS": numero_CTPS,"senha_funcionario": senha_funcionario}
+        return {"nome": nome, "data_nascimento": data_nascimento, "cpf": cpf, "numero_CTPS": numero_CTPS, "senha_funcionario": senha_funcionario}
+
+    def pega_dados_funcionario_alteracao(self):
+        print("-------- Dados Atualizados do Funcionário ----------")
+        nome = input("Nome: ")
+        senha_funcionario = input("Senha para operações: ")
+
+        return {"nome": nome, "senha_funcionario": senha_funcionario}
+
+    def seleciona_cpf(self):
+        return cpf_valido()
+
+    def pega_senha_pessoa(self):
+        return input("Digite a sua senha para prosseguir com a operação: ")
+
+    def lista_cliente(self, dados_cliente):
+        print("Cliente:", dados_cliente["codigo"], "-", dados_cliente["nome"])
+        print("Data de nascimento do cliente:", dados_cliente["data_nascimento"])
+        print("CPF do cliente:", str(dados_cliente["cpf"])[0:3], ".", str(dados_cliente["cpf"])[3:6], ".", str(dados_cliente["cpf"])[6:9], "-", str(dados_cliente["cpf"])[9:11])
+        print()
+
+    def lista_funcionario(self, dados_funcionario):
+        print("Funcionário:", dados_funcionario["codigo"], "-", dados_funcionario["nome"])
+        print("Data de nascimento do funcinário:", dados_funcionario["data_nascimento"])
+        print("CPF do funcinário:", str(dados_funcionario["cpf"])[0:3], ".", str(dados_funcionario["cpf"])[3:6], ".", str(dados_funcionario["cpf"])[6:9], "-", str(dados_funcionario["cpf"])[9:11])
+        print("Número CTPS do funcinário", dados_funcionario["numero_CTPS"])
+        print()
+
+    def mostra_mensagem(self, msg):
+        print(msg)
